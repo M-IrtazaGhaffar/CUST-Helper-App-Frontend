f9218919a3e5fa53c2055e94cf7c06b4bee38efe
Muhammad Irtaza Ghaffar <93589223+M-IrtazaGhaffar@users.noreply.github.com>
2024-05-20 05:10:24 +0000

    1111

--- src/screens/Gemini.jsx
+++ src/screens/Gemini.jsx
@@ -1,20 +1,25 @@
-import { Box, Button, Input } from "@chakra-ui/react"
+import { Box, Button, Flex, Input } from "@chakra-ui/react"
 import { useState } from "react"
 import { GoogleGenerativeAI } from "@google/generative-ai"
 
 function Gemini() {
     const [key, setKey] = useState("AIzaSyDRRr9eVQYxt4BGVGTXf9EswJ9onfECWCQ")
     const [query, setQuery] = useState("")
+    const [convo, setConvo] = useState([])
 
-    const ai = async () => {
+    const ai = async (e) => {
+        e.preventDefault()
         const genAI = new GoogleGenerativeAI(key);
 
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"});
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
 
         const result = await model.generateContent([
-          "What is Pakistan?"
+            query
         ]);
-        console.log(result.response.text());
+        setConvo([...convo, {
+            q: query,
+            a: result.response.text()
+        }])
     }
 
     return (
@@ -28,8 +33,35 @@ function Gemini() {
             justifyContent='center'
             flexDirection='column'
         >
-            <Input bgColor='white' placeholder="Ask Something!" onChange={(e) => setQuery(e.target.value)} />
-            <Button onClick={ai}>Ask</Button>
+            {
+                convo.map((i, ind) => {
+                    return (
+                        <Flex gap='5px' flexDirection='column' padding='10px' >
+                            <Box maxWidth='80%' position='relative' right='0' >
+                                <Box fontSize='xs' maxWidth='96' >Query</Box>
+                                <Box width='fit-content' bgColor='white' borderTopRadius='lg' borderLeftRadius='lg' padding='10px' >{i.q}</Box>
+                            </Box>
+                            <Box maxWidth='80%'>
+                                <Box fontSize='xs' >Result</Box>
+                                <Box width='fit-content' bgColor='white' borderTopRadius='lg' borderRightRadius='lg' padding='10px'>{i.a}</Box>
+                            </Box>
+                        </Flex>
+                    )
+                })
+            }
+
+            <form onSubmit={ai} style={{
+                position: 'absolute',
+                top: '89%',
+                display: 'flex',
+                justifyContent: 'space-between',
+                width: '100%',
+                gap: '10px',
+                padding: '10px'
+            }}>
+                <Input bgColor='white' placeholder="Ask Something!" onChange={(e) => setQuery(e.target.value)}  />
+                <Button onClick={ai}>Ask</Button>
+            </form>
         </Box>
     )
 }
